b2fd6447566547f28d70876955bc8684c0b0d1a1
Bill Lixfield <w.lixfield@gmail.com>
2022-10-08 10:58:27 -0400

    Add playable functionality

--- rps.js
+++ rps.js
@@ -10,5 +10,24 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "Rock") {
+        if (computerSelection === "Paper") { return "You Lose! Paper beats Rock" }
+        if (computerSelection === "Scissors") { return "You Win! Rock beats Scissors"}
+    } else if (playerSelection === "Paper") {
+        if (computerSelection === "Rock") { return "You Win! Paper beats Rock" }
+        if (computerSelection === "Scissors") { return "You Lose! Scissors beats Paper" }
+    } else if (playerSelection === "Scissors") {
+        if (computerSelection === "Rock") { return "You Lose! Rock beats Scissors" }
+        if (computerSelection === "Paper") { return "You Win! Scissors beats Paper" }
+    } else if (playerSelection === computerSelection) { return "Tie Game! Try Again" }
+    else { return "Not a valid option. Pick Rock, Paper, or Scissors."}
+}
+
+function game() {
+    
+}
+
+const playerSelection = "Rock"
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
